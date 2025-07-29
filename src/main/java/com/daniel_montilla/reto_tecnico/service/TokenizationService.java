07233c099f1572c1c99b546fbce654c7ced763d1
Daniel Montilla <montilla.d2000@gmail.com>
2025-07-29 14:58:17 -0400

    refactor: consolidate TokenizationService implementation into a single class

--- src/main/java/com/daniel_montilla/reto_tecnico/service/TokenizationService.java
+++ src/main/java/com/daniel_montilla/reto_tecnico/service/TokenizationService.java
@@ -1,7 +1,41 @@
 package com.daniel_montilla.reto_tecnico.service;
 
 import com.daniel_montilla.reto_tecnico.dto.CreditCardRequest;
+import com.daniel_montilla.reto_tecnico.exception.TokenizationRejectedException;
+import com.daniel_montilla.reto_tecnico.exception.TokenGenerationException;
 
-public interface TokenizationService {
-  String tokenizeCard(CreditCardRequest creditCardRequest);
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+@Service
+public class TokenizationService {
+
+  @Value("${tokenization.rejection.probability}")
+  private double rejectionProbability;
+
+  @Value("${tokenization.hmac.secret-key}")
+  private String hmacSecretKey;
+
+  private static final String HMAC_ALGORITHM = "HmacSHA256";
+
+  public String tokenizeCard(CreditCardRequest creditCardRequest) {
+
+    if (Math.random() < rejectionProbability) {
+      throw new TokenizationRejectedException();
+    }
+
+    String canonicalString = creditCardRequest.toCanonicalString();
+
+    try {
+      Mac mac = Mac.getInstance(HMAC_ALGORITHM);
+      mac.init(new SecretKeySpec(hmacSecretKey.getBytes(), HMAC_ALGORITHM));
+      byte[] hmac = mac.doFinal(canonicalString.getBytes());
+      return Base64.getEncoder().encodeToString(hmac);
+    } catch (Exception e) {
+      throw new TokenGenerationException("Failed to generate HMAC token", e);
+    }
+  }
 }
