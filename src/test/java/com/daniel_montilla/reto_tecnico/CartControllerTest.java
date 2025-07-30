1bb45fdafb86a12adca24787cc96bd6888fcbe38
Daniel Montilla <montilla.d2000@gmail.com>
2025-07-30 15:59:04 -0400

    test: add maven-surefire-plugin configuration to pom.xml and remove DirtiesContext annotation from CartControllerTest

--- src/test/java/com/daniel_montilla/reto_tecnico/CartControllerTest.java
+++ src/test/java/com/daniel_montilla/reto_tecnico/CartControllerTest.java
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -37,7 +36,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class CartControllerTest {
 
 	@Autowired
